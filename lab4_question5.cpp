f5378d2e0d1049ff90df638491e1470d5196d64d
KishanDeka <31157122+KishanDeka@users.noreply.github.com>
2017-09-15 18:40:58 +0530

    Update lab4_question5.cpp

--- lab4_question5.cpp
+++ lab4_question5.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 void check(int a)
-{ int m, r, k, c, d, q, n, t, e, f, g;
+{ int m, r, k, c, d, q, n, t, e, f, g, I, M, N, R, C;
   k=0;
   for (e=2;e<=a/2;e++) { r=a%e;
                         if (r==0) {k++;}
@@ -12,7 +12,7 @@ void check(int a)
   q=0;
   t=0;
   n=a;
-  while (n>0) { 
+  while (n>0) {
                 n=n/10;
                 t++;
               }
@@ -20,9 +20,22 @@ void check(int a)
                   d=d/10;
                   g=1;
                   for (f=1;f<=t;f++)
-                      { g=c*g;} 
+                      { g=c*g;}
                   q=g+q; }
   if (q==a) {cout << "The number is an armstrong number."<<endl;}
+  	C=a;
+  	M=0;
+  	for(I=2; I<=C; I++)
+	   { R=C%I;
+	     N=C/I;
+	     switch (R)
+	     {
+	         case 0: M=M+N;
+	     }
+
+       }
+   if (M==a) {cout <<"The number is a perfect number."<<endl;}
+
 }
 int main ()
 { int x;
