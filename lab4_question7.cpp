a5a1f23a6b40dbb19d9dcb481781450d45a10045
KishanDeka <31157122+KishanDeka@users.noreply.github.com>
2017-09-15 12:17:29 +0530

    Create lab4_question7.cpp

---
+++ lab4_question7.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void armstrong(int a, int b)
+{ int c, d, f, g, q, n, t, r;
+  for (c=a+1;c<b;c++)
+  { n=c;
+    t=0;
+    q=0;
+    d=c;
+    while (n>0) { n=n/10;
+                  t++;
+                }
+
+    while (d>0) { r=d%10;
+                  d=d/10;
+                  g=1;
+                  for (f=1;f<=t;f++)
+                      { g=r*g;}
+                  q=g+q; }
+    if (q==c) {cout << c<<endl;}
+  }
+}
+int main () 
+{   int x, y;
+    cout << "Enter the endpoints of the interval : ";
+    cin >> x>>y;
+    armstrong(x,y);
+return 0;
+}
